3c6dddc99fefbca2c2ab2af5cc59f6f2f28d8ce8
vutheanh1611 <vutheanh121216112000@gmail.com>
2022-10-21 21:35:21 +0700

    assigment5

---
+++ src/assigment/ContactController.java
@@ -0,0 +1,35 @@
+package assigment;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ContactController{
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtAddress;
+    public TextField txtPhone;
+
+    public TextField txtAge;
+    public Text strName;
+    public Text strEmail;
+    public Text strAddress;
+    public Text strPhone;
+
+    public Text strAge;
+
+    public void submit(ActionEvent actionEvent) {
+        String Name = txtName.getText();
+        String Email = txtEmail.getText();
+        String Address = txtAddress.getText();
+        String Phone = txtPhone.getText();
+        String Age = txtAge.getText();
+
+
+        strName.setText(Name);
+        strEmail.setText(Email);
+        strAddress.setText(Address);
+        strPhone.setText(Phone);
+        strAge.setText(Age);
+    }
+}
